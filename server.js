e2746f836aca8d214adb81a7a7a7a62827883651
hnekhniadovich <hanna@nextguest.com>
2020-06-08 13:59:10 -0400

    bug fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-if(process.env.NODE_ENV !== 'production') require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
